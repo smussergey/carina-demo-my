6ebab75ca210eb8670950116bb4c6a5d6a76c66b
Sergii Smus <smussergey@gmail.com>
2022-10-21 11:00:55 +0300

    WebSampleTest is rewritten

--- src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -39,56 +39,55 @@ import java.util.List;
  * @author qpsdemo
  */
 public class WebSampleTest implements IAbstractTest {
-//    @Test()
-//    @MethodOwner(owner = "qpsdemo")
-//    @TestPriority(Priority.P3)
-//    @TestLabel(name = "feature", value = {"web", "regression"})
-//    public void testModelSpecs() {
-//        // Open GSM Arena home page and verify page is opened
-//        HomePage homePage = new HomePage(getDriver());
-//        homePage.open();
-//        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-//
-//        //Closing advertising if it's displayed
-//        homePage.getWeValuePrivacyAd().closeAdIfPresent();
-//
-//        // Select phone brand
-//        homePage = new HomePage(getDriver());
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P3)
+    @TestLabel(name = "feature", value = {"web", "regression"})
+    public void testModelSpecs() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+
+        homePage.getWeValuePrivacyAd().closeAdIfPresent();
+
+        homePage = new HomePage(getDriver());
 //        BrandModelsPage productsPage = homePage.selectBrand("Samsung");
-//        // Select phone model
+        BrandModelsPage brandModelsPage = homePage.selectBrand("Samsung");
+
 //        ModelInfoPage productInfoPage = productsPage.selectModel("Galaxy A52 5G");
-//        // Verify phone specifications
-//        SoftAssert softAssert = new SoftAssert();
-//        softAssert.assertEquals(productInfoPage.readDisplay(), "6.5\"", "Invalid display info!");
-//        softAssert.assertEquals(productInfoPage.readCamera(), "64MP", "Invalid camera info!");
-//        softAssert.assertEquals(productInfoPage.readRam(), "6/8GB RAM", "Invalid ram info!");
-//        softAssert.assertEquals(productInfoPage.readBattery(), "4500mAh", "Invalid battery info!");
-//        softAssert.assertAll();
-//    }
-//
-//
-//    @Test()
-//    @MethodOwner(owner = "qpsdemo")
-//    @TestPriority(Priority.P1)
-//    @TestLabel(name = "feature", value = {"web", "acceptance"})
-//    public void testCompareModels() {
-//        // Open GSM Arena home page and verify page is opened
-//        HomePage homePage = new HomePage(getDriver());
-//        homePage.open();
-//        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
-//        // Open model compare page
-//        FooterMenu footerMenu = homePage.getFooterMenu();
-//        Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
-//        CompareModelsPage comparePage = footerMenu.openComparePage();
-//        // Compare 3 models
-//        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
-//        // Verify model announced dates
-//        SoftAssert softAssert = new SoftAssert();
-//        softAssert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2016, March 31");
-//        softAssert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June 19");
-//        softAssert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
-//        softAssert.assertAll();
-//    }
+        ModelInfoPage modelInfoPage = brandModelsPage.selectModel("Galaxy A52 5G");
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(modelInfoPage.readDisplay(), "6.5\"", "Invalid display info!");
+        softAssert.assertEquals(modelInfoPage.readCamera(), "64MP", "Invalid camera info!");
+        softAssert.assertEquals(modelInfoPage.readRam(), "6/8GB RAM", "Invalid ram info!");
+        softAssert.assertEquals(modelInfoPage.readBattery(), "4500mAh", "Invalid battery info!");
+        softAssert.assertAll();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    @TestLabel(name = "feature", value = {"web", "acceptance"})
+    public void testCompareModels() {
+
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+
+        FooterMenu footerMenu = homePage.getFooterMenu();
+        Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
+        CompareModelsPage comparePage = footerMenu.openComparePage();
+
+        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
+        // Verify model announced dates
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2016, March 31");
+        softAssert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June 19");
+        softAssert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
+        softAssert.assertAll();
+    }
 
     @Test()
     @MethodOwner(owner = "qpsdemo")
@@ -101,25 +100,17 @@ public class WebSampleTest implements IAbstractTest {
         NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
         Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
 
+        final String searchQ = "iphone";
+        List<NewsItem> news = newsPage.searchNews(searchQ);
+        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
+
+        SoftAssert softAssert = new SoftAssert();
+        for (NewsItem n : news) {
+            System.out.println(n.readTitle());
+            softAssert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ),
+                    "Invalid search results for " + n.readTitle());
+        }
+        softAssert.assertAll();
     }
 
-//    public void testNewsSearch() {
-//        HomePage homePage = new HomePage(getDriver());
-//        homePge.open();
-//        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
-//
-//        NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
-//        Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
-//
-//        final String searchQ = "iphone";
-//        List<NewsItem> news = newsPage.searchNews(searchQ);
-//        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
-//        SoftAssert softAssert = new SoftAssert();
-//        for(NewsItem n : news) {
-//            System.out.println(n.readTitle());
-//            softAssert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ),
-//                    "Invalid search results for " + n.readTitle());
-//        }
-//        softAssert.assertAll();
-//    }
 }
